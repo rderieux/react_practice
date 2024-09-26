6fd472fa3db3df35f00deddd671b2c173f59201f
Robert DeRieux <ppldiecomplex@gmail.com>
2024-09-26 10:08:19 -0600

    Create ContactList component and add a JSX table

---
+++ src/contact_list/components/ContactList.jsx
@@ -0,0 +1,21 @@
+export default function ContactList() {
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>Contact List</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Phone</td>
+          </tr>
+          <tr>{}</tr>
+        </tbody>
+      </table>
+    </>
+  );
+}
